90b8b7208d692765d0f6269c8ff03fe1bfc7e12c
wangrenyu <wrjade@163.com>
2025-09-16 11:21:26 +0800

    update

--- flake.nix
+++ flake.nix
@@ -2,46 +2,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    rust-overlay,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [(import rust-overlay)];
           config.allowUnfree = true;
           config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [ "gurobi" "gurobipy" ];
         };
       in {
         formatter = pkgs.alejandra;
-        devShells.default = import ./shell.nix {inherit pkgs;};
-
+        
         packages = {
-          reaslab-proto = import ./pkgs/reaslab-proto.nix {inherit pkgs;};
-
-          reaslab-be = import ./pkgs/reaslab-be.nix {
-            inherit pkgs;
-            reaslab-proto = self.outputs.packages."${system}".reaslab-proto;
-          };
-
-          reaslab-be-image = import ./pkgs/reaslab-be-image.nix {
-            inherit pkgs;
-            reaslab-be = self.outputs.packages."${system}".reaslab-be;
-          };
-
           # Docker image package for CI/CD
           docker-image = import ./docker.nix {inherit pkgs;};
         };
